686db0de610dcbcd8767433194695e5bd16caab1
Adexstar <fuhadademola35@gmail.com>
2026-01-26 18:16:50 +0100

    Hybrid Conversion: Add Vite infrastructure for Lovable compatibility

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from '../apps/web/afrimine-solar-earn-main/src/App';
+import '../apps/web/afrimine-solar-earn-main/src/index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
